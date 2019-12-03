007d2d67d22df41a701b4df9b12aa03cc8efa28b
Ryan Nutt <ryan@nutt.com>
2019-12-03 13:16:07 -0600

    add GitHub updater from SmashingMagazine

--- dwqa-comments.php
+++ dwqa-comments.php
@@ -3,7 +3,7 @@
  * Plugin Name:       DWQA Comments
  * Plugin URI:        https://www.nutt.net
  * Description:       Replace WordPress comments with DW Q&A Plugin
- * Version:           0.0.1
+ * Version:           0.1.0
  * Requires at least: 5.2
  * Requires PHP:      7.2
  * Author:            Ryan Nutt
@@ -14,3 +14,11 @@
  */
 
 include(__DIR__ . '/_inc/class.dwcomments.php');
+
+if( ! class_exists( 'Smashing_Updater' ) ){
+	include_once( plugin_dir_path( __FILE__ ) . '_inc/updater.php' );
+}
+$updater = new Smashing_Updater( __FILE__ );
+$updater->set_username( 'ryannutt' );
+$updater->set_repository( 'dwqa-wordpress-comments' );
+$updater->initialize();
